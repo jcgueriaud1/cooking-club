4f919bcd789f10fe48d21bcdbc81a9e85501648c
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2021-06-21 21:50:50 +0300

    Step 3

--- src/main/java/com/example/application/data/service/EventRepository.java
+++ src/main/java/com/example/application/data/service/EventRepository.java
@@ -3,8 +3,6 @@ package com.example.application.data.service;
 import com.example.application.data.entity.Event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.time.LocalDateTime;
-import javax.annotation.Nullable;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
 
